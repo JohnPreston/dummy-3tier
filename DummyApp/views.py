f943f19de93dd3c9ea5784b3704a6bb291946565
John Mille <john@ews-network.net>
2016-06-22 11:24:54 +0000

    Hello world rendered

--- DummyApp/views.py
+++ DummyApp/views.py
@@ -1,9 +1,8 @@
 import os, json
 from DummyApp import DummyApp
 
-from flask import Flask, jsonify, request, make_response
+from flask import Flask, jsonify, request, make_response, render_template
 
 @DummyApp.route('/')
 def hello():
-    return "Hello World"
-
+    return render_template('index.j2')
