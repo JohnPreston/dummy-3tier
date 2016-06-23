7d5e7cf91fc731800af7e5748f727b7cfe6a4833
John Mille <john@ews-network.net>
2016-06-23 09:54:54 +0000

    NGINX to come

--- install_env.sh
+++ install_env.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 git clone https://github.com/johnpreston/dummy-3tier ~/dummy-3tier
-sudo yum install mysql mysql-devel gcc python-pip python-virtualenv -y
+sudo yum install mysql mysql-devel gcc python-pip python-virtualenv -y MySQL-python
 sudo pip install pip --upgrade
 cd ~/dummy-3tier
 virtualenv dummy_env
@@ -9,3 +9,5 @@ source dummy_env/bin/activate
 pip install flask Flask-WTF flask-sqlalchemy requests gunicorn supervisor mysql-python
 supervisord -c ~/dummy-3tier/supervisord.conf
 
+pip install nginx -y
+# Script the import of the NGINX configuration file
